a1d8976114a1d7fbfbe226c0fed89abb2a02c4ad
Robin Joseph <robinjoseph11998@gmail.com>
2023-11-01 00:08:21 +0530

    Dependency injection done

--- pkg/di/wire.go
+++ pkg/di/wire.go
@@ -5,30 +5,18 @@ package di
 
 import (
 	"Todo/pkg/api/handlers"
-	"Todo/pkg/db"
 	"Todo/pkg/repository"
 	"Todo/pkg/usecase"
 
 	"github.com/google/wire"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func InitializeHandlerApi() *handlers.Handler {
 	wire.Build(
-		db.ConnectDB,
-		ProvideMongoCollection,
 		handlers.NewHandler,
-		repository.NewTodoRepository,
 		usecase.NewUsecase,
+		repository.NewTodoRepository,
 	)
 	return &handlers.Handler{}
 
-}
-
-func ProvideMongoCollection() (*mongo.Collection, error) {
-	collection, err := db.CreateMongoCollection("mydb", "Todo")
-	if err != nil {
-		return nil, err
-	}
-	return collection, nil
 }
