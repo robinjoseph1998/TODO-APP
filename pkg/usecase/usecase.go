88f28a9cc548d8dcdb2f1a0094131f98a1cacee5
Robin Joseph <robinjoseph11998@gmail.com>
2023-11-17 01:28:05 +0530

    changes

--- pkg/usecase/usecase.go
+++ pkg/usecase/usecase.go
@@ -18,7 +18,7 @@ func NewUsecase(Repo repo.RepoInterfaces) use.UseCaseInterface {
 func (uu *usecase) ExecuteAddName(request models.Test) (string, error) {
 	SavedName, err := uu.Repo.CreateName(request)
 	if err != nil {
-		return "", errors.New("Can't added name")
+		return "", errors.New("can't add name")
 	}
 	return SavedName, nil
 }
